52c7f2abe78c072439483029ee24a4223ee0861f
Edward Moyse <edward.moyse@cern.ch>
2020-07-01 08:20:46 +0200

    Temporarily disable JSON loading, and use JiveXML instead. Problem is that Tracks are incomplete, so probably need RK before making this the default.

--- src/app/sections/atlas/atlas.component.ts
+++ src/app/sections/atlas/atlas.component.ts
@@ -3,6 +3,8 @@ import { EventdisplayService } from '../../services/eventdisplay.service';
 import { Configuration } from '../../services/extras/configuration.model';
 import { PresetView } from '../../services/extras/preset-view.model';
 import { HttpClient } from '@angular/common/http';
+import { JiveXMLLoader } from '../../services/loaders/jivexml-loader';
+import { HttpHeaders } from '@angular/common/http';
 
 
 @Component({
@@ -11,12 +13,13 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./atlas.component.scss']
 })
 export class AtlasComponent implements OnInit {
+  jivexmlloader: JiveXMLLoader;
 
   constructor(private eventDisplay: EventdisplayService, private http: HttpClient) {
   }
 
   ngOnInit() {
-
+    this.jivexmlloader = new JiveXMLLoader();
     const configuration = new Configuration();
     configuration.presetViews = [
       new PresetView('Left View', [0, 0, -12000], 'left-cube'),
@@ -25,8 +28,22 @@ export class AtlasComponent implements OnInit {
     ];
 
     this.eventDisplay.init(configuration);
-    this.http.get('assets/files/event_data/atlaseventdump2.json')
-      .subscribe((res: any) => this.eventDisplay.parsePhoenixEvents(res));
+    // this.http.get('assets/files/event_data/atlaseventdump2.json')
+    //   .subscribe((res: any) => this.eventDisplay.parsePhoenixEvents(res));
+
+    this.http.get('assets/files/JiveXML/JiveXML_336567_2327102923.xml', {  
+      headers: new HttpHeaders()  
+        .set('Content-Type', 'text/xml')  
+        .append('Access-Control-Allow-Methods', 'GET')  
+        .append('Access-Control-Allow-Origin', '*')  
+        .append('Access-Control-Allow-Headers', "Access-Control-Allow-Headers, Access-Control-Allow-Origin, Access-Control-Request-Method"),  
+      responseType: 'text'  
+    }).subscribe((data: any) => {
+      this.jivexmlloader.process(data);
+      const eventData = this.jivexmlloader.getEventData();
+      this.eventDisplay.buildEventDataFromJSON(eventData);
+    });
+
     this.eventDisplay.loadOBJGeometry('assets/geometry/ATLAS/toroids.obj', 'Toroids', 0x8c8c8c, false);
     this.eventDisplay.loadOBJGeometry('assets/geometry/ATLAS/TRT.obj', 'TRT', 0x356aa5, false);
     this.eventDisplay.loadOBJGeometry('assets/geometry/ATLAS/SCT.obj', 'SCT', 0xfff400, false);
